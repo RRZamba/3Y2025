1c178eb90b2034b688e35285cd69ea950fc9a3e2
RRZamba <114082625+RRZamba@users.noreply.github.com>
2025-02-21 07:27:47 -0300

    Add files via upload

---
+++ 01_Aula_Revisao_Mil_Grau/doFormularioShow.php
@@ -0,0 +1,14 @@
+<?php
+	//Recebendo os dados do form
+	$nominho = $_POST['nome'];
+	$sobrenominho = $_POST['sobrenome'];
+	$nomemae = $_POST['mae'];
+
+	//Testando 
+	//echo "$nominho <br>";
+	//echo "$sobrenominho <br>";
+	//echo "$nomemae <br>";
+	
+	
+
+?>
